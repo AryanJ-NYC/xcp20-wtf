fdd56379b6b204482631b853a5b0308e7aee45e0
Aryan Jabbari <AryanJabbari@gmail.com>
2023-05-30 16:40:24 -0400

    Add percentage overminted

--- src/app/homepage/Xcp20Table.tsx
+++ src/app/homepage/Xcp20Table.tsx
@@ -9,11 +9,30 @@ export const Xcp20Table = async () => {
         next: { revalidate: revliadationInterval },
       });
       const { data } = await resp.json();
+
+      const mempoolResp = await fetch(
+        `https://mempool.space/api/address/${xcp20Details.mintAddress}`,
+        { next: { revalidate: revliadationInterval } }
+      );
+      const { chain_stats } = await mempoolResp.json();
+      const { funded_txo_sum } = chain_stats;
+      const btcSent = funded_txo_sum / 100_000_000;
+
       const datum = data[0];
       const soldCount = Number(datum.escrow_quantity) - Number(datum.give_remaining);
-      const btcBurnt = (datum.satoshi_price * (soldCount / Number(datum.give_quantity))).toFixed(8);
+      const btcBurnt = datum.satoshi_price * (soldCount / Number(datum.give_quantity));
       const percentMinted = ((soldCount / datum.escrow_quantity) * 100).toFixed(2);
-      return { name: xcp20Name, btcBurnt, percentMinted };
+
+      return {
+        name: xcp20Name,
+        btcBurnt: btcBurnt.toFixed(8),
+        btcSent,
+        percentMinted,
+        percentOverminted:
+          Number(datum.give_remaining) <= 0
+            ? ((Number(btcSent) / Number(btcBurnt)) * 100).toFixed(2)
+            : 0,
+      };
     })
   );
 
@@ -26,12 +45,13 @@ export const Xcp20Table = async () => {
         <thead>
           <tr>
             <th className="dark:text-slate-300">Name</th>
-            <th className="dark:text-slate-300">BTC Burned</th>
-            <th className="dark:text-slate-300">Percent Minted</th>
+            <th className="dark:text-slate-300 text-right">BTC Burned</th>
+            <th className="dark:text-slate-300 text-right">Percent Minted</th>
+            <th className="dark:text-slate-300 text-right">Percent Overminted</th>
           </tr>
         </thead>
         <tbody className="divide-y divide-slate-800 dark:divide-slate-200">
-          {xcp20sWithMintPercentage.map(({ btcBurnt, name, percentMinted }) => (
+          {xcp20sWithMintPercentage.map(({ btcBurnt, name, percentMinted, percentOverminted }) => (
             <tr key={name}>
               <td>
                 <A href={`https://xchain.io/asset/${name}`} target="_blank">
@@ -40,12 +60,16 @@ export const Xcp20Table = async () => {
               </td>
               <td className="text-right">{btcBurnt}</td>
               <td className="text-right">{percentMinted}%</td>
+              <td className="text-right">{percentOverminted}%</td>
             </tr>
           ))}
         </tbody>
       </table>
       <H2>Total BTC Burned</H2>
-      <p>{xcp20sWithMintPercentage.reduce((acc, curr) => acc + Number(curr.btcBurnt), 0)} BTC</p>
+      <p>
+        {xcp20sWithMintPercentage.reduce((acc, curr) => acc + Number(curr.btcSent), 0).toFixed(4)}{' '}
+        BTC
+      </p>
     </div>
   );
 };
